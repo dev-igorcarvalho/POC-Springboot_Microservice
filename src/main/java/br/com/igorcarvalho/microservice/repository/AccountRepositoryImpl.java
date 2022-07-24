f570e5031e0483e739f6589fa8020a941767ba3d
Igor Carvalho <igor.carvalho@datagrupo.com.br>
2022-07-24 15:44:23 -0300

    FEAT: Base skeleton  Adding a repositories adapters TASK: #ref

---
+++ src/main/java/br/com/igorcarvalho/microservice/repository/AccountRepositoryImpl.java
@@ -0,0 +1,20 @@
+package br.com.igorcarvalho.microservice.repository;
+
+import br.com.igorcarvalho.microservice.model.entity.AccountEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AccountRepositoryImpl extends AccountRepository, JpaRepository<AccountEntity, Long> {
+    @Override
+    default AccountEntity save(AccountEntity entity) {
+        return save(entity);
+    }
+
+    @Override
+    default Optional<AccountEntity> find(Long id) {
+        return findById(id);
+    }
+}
---
+++ src/main/java/br/com/igorcarvalho/microservice/repository/TransactionRepositoryImpl.java
@@ -0,0 +1,13 @@
+package br.com.igorcarvalho.microservice.repository;
+
+import br.com.igorcarvalho.microservice.model.entity.TransactionEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepositoryImpl extends TransactionRepository, JpaRepository<TransactionEntity, Long> {
+    @Override
+    default TransactionEntity save(TransactionEntity entity) {
+        return save(entity);
+    }
+}

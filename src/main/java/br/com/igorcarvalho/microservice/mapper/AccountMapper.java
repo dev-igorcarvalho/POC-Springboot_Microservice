61cbd56091fd3881fe271456c8eab5b061a4aa28
Igor Carvalho <igor.carvalho@datagrupo.com.br>
2022-07-25 11:09:13 -0300

    FEAT: Accounts  Adding actual implementation of AccountMapper  Adding actual implementation of TransactionMapper TASK: #ref

--- src/main/java/br/com/igorcarvalho/microservice/mapper/AccountMapper.java
+++ src/main/java/br/com/igorcarvalho/microservice/mapper/AccountMapper.java
@@ -2,31 +2,38 @@ package br.com.igorcarvalho.microservice.mapper;
 
 import br.com.igorcarvalho.microservice.model.dto.AccountDto;
 import br.com.igorcarvalho.microservice.model.entity.AccountEntity;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 @Qualifier("accountMapper")
+@RequiredArgsConstructor
 public class AccountMapper implements Mapper<AccountEntity, AccountDto> {
+    private final TransactionMapper transactionMapper;
+
     @Override
-    public AccountEntity toEntity(AccountDto accountDto) {
-        throw new UnsupportedOperationException("Not implemented yet.");
+    public AccountEntity toEntity(AccountDto dto) {
+        return AccountEntity.builder().id(dto.getId()).documentNumber(dto.getDocumentNumber()).transactions(transactionMapper.toEntity(dto.getTransactions())).build();
     }
 
     @Override
-    public AccountDto toDto(AccountEntity accountEntity) {
-        throw new UnsupportedOperationException("Not implemented yet.");
+    public AccountDto toDto(AccountEntity entity) {
+        return AccountDto.builder().id(entity.getId()).documentNumber(entity.getDocumentNumber()).transactions(transactionMapper.toDto(entity.getTransactions())).build();
     }
 
     @Override
-    public List<AccountEntity> toEntity(List<AccountDto> accountDtos) {
-        throw new UnsupportedOperationException("Not implemented yet.");
+    public List<AccountEntity> toEntity(List<AccountDto> dtos) {
+        List<AccountEntity> result = dtos.stream().map(e -> this.toEntity(e)).collect(Collectors.toList());
+        return result;
     }
 
     @Override
-    public List<AccountDto> toDto(List<AccountEntity> accountEntities) {
-        throw new UnsupportedOperationException("Not implemented yet.");
+    public List<AccountDto> toDto(List<AccountEntity> entities) {
+        List<AccountDto> result = entities.stream().map(e -> this.toDto(e)).collect(Collectors.toList());
+        return result;
     }
 }
--- src/main/java/br/com/igorcarvalho/microservice/mapper/TransactionMapper.java
+++ src/main/java/br/com/igorcarvalho/microservice/mapper/TransactionMapper.java
@@ -1,32 +1,48 @@
 package br.com.igorcarvalho.microservice.mapper;
 
+import br.com.igorcarvalho.microservice.model.dto.OperationTypeDto;
 import br.com.igorcarvalho.microservice.model.dto.TransactionDto;
 import br.com.igorcarvalho.microservice.model.entity.TransactionEntity;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 @Qualifier("transactionMapper")
 public class TransactionMapper implements Mapper<TransactionEntity, TransactionDto> {
     @Override
-    public TransactionEntity toEntity(TransactionDto transactionDto) {
-        throw new UnsupportedOperationException("Not implemented yet.");
+    public TransactionEntity toEntity(TransactionDto dto) {
+        return TransactionEntity.builder()
+              .id(dto.getId())
+              .amount(dto.getAmount())
+              .eventDate(dto.getEventDate())
+//              .operationTypeEntity()
+              .build();
     }
 
     @Override
-    public TransactionDto toDto(TransactionEntity transactionEntity) {
-        throw new UnsupportedOperationException("Not implemented yet.");
+    public TransactionDto toDto(TransactionEntity entity) {
+        return TransactionDto.builder()
+              .id(entity.getId())
+              .amount(entity.getAmount())
+              .eventDate(entity.getEventDate())
+              .operationTypeDto(
+                    new OperationTypeDto(entity.getOperationType().getId(),
+                          entity.getOperationType().getDscription()))
+              .build();
     }
 
     @Override
-    public List<TransactionEntity> toEntity(List<TransactionDto> transactionDtos) {
-        throw new UnsupportedOperationException("Not implemented yet.");
+    public List<TransactionEntity> toEntity(List<TransactionDto> dtos) {
+        List<TransactionEntity> result = dtos.stream().map(e -> this.toEntity(e)).collect(Collectors.toList());
+        return result;
     }
 
     @Override
-    public List<TransactionDto> toDto(List<TransactionEntity> transactionEntities) {
-        throw new UnsupportedOperationException("Not implemented yet.");
+    public List<TransactionDto> toDto(List<TransactionEntity> entities) {
+        List<TransactionDto> result = entities.stream().map(e -> this.toDto(e)).collect(Collectors.toList());
+        return result;
     }
 }

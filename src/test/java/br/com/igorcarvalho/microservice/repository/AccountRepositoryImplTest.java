6d862d2b525ae75b833e1ea9b972f94e2dc08057
Igor Carvalho <igor.carvalho@datagrupo.com.br>
2022-07-25 14:50:15 -0300

    TEST: Repositories  Adding account repo test  Adding transaction repo test TASK: #ref

--- src/test/java/br/com/igorcarvalho/microservice/repository/AccountRepositoryImplTest.java
+++ src/test/java/br/com/igorcarvalho/microservice/repository/AccountRepositoryImplTest.java
@@ -1,14 +1,66 @@
 package br.com.igorcarvalho.microservice.repository;
 
+import br.com.igorcarvalho.microservice.model.entity.AccountEntity;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@DisplayName("Account Repository tests")
 class AccountRepositoryImplTest {
 
+    @Autowired
+    private AccountRepository repository;
+
     @Test
-    void save() {
+    @DisplayName("Should persist a new account")
+    void save_should_persist_a_valid_new_account() {
+        //given
+        AccountEntity toBeSaved = AccountEntity.builder().documentNumber("12345678900").build();
+        //when
+        AccountEntity saved = repository.create(toBeSaved);
+        //then
+        assertAll(
+              () -> assertNotNull(saved),
+              () -> assertNotNull(saved.getId()),
+              () -> assertNotNull(saved.getDocumentNumber()),
+              () -> assertEquals(toBeSaved.getDocumentNumber(), saved.getDocumentNumber())
+        );
     }
 
     @Test
-    void find() {
+    @DisplayName("Should retrieve an existing account by its id")
+    void find_should_retrieve_a_account() {
+        //given
+        AccountEntity toBeSaved = AccountEntity.builder().documentNumber("12345678900").build();
+        repository.create(toBeSaved);
+        //when
+        Optional<AccountEntity> optional = repository.find(toBeSaved.getId());
+
+        //then
+        assertAll(
+              () -> assertTrue(optional.isPresent()),
+              () -> assertNotNull(optional.get()),
+              () -> assertNotNull(optional.get().getId()),
+              () -> assertNotNull(optional.get().getDocumentNumber()),
+              () -> assertEquals(toBeSaved.getDocumentNumber(), optional.get().getDocumentNumber()),
+              () -> assertDoesNotThrow(() -> new Exception())
+        );
+    }
+
+    @Test
+    @DisplayName("Should not find any account when the passed id is not valid")
+    void find_should_not_retrieve_an_account() {
+        //given
+        final long invalidId = 7L;
+        //when
+        Optional<AccountEntity> optional = repository.find(invalidId);
+        //then
+        assertTrue(optional.isEmpty());
     }
 }
--- src/test/java/br/com/igorcarvalho/microservice/repository/TransactionRepositoryImplTest.java
+++ src/test/java/br/com/igorcarvalho/microservice/repository/TransactionRepositoryImplTest.java
@@ -1,10 +1,41 @@
 package br.com.igorcarvalho.microservice.repository;
 
+import br.com.igorcarvalho.microservice.model.entity.TransactionEntity;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@DisplayName("Transaction Repository tests")
 class TransactionRepositoryImplTest {
 
+    @Autowired
+    private TransactionRepository repository;
+
     @Test
-    void save() {
+    @DisplayName("Should persist a new transaction")
+    void save_should_persist_a_valid_new_transaction() {
+        //given
+        TransactionEntity toBeSaved = TransactionEntity.builder()
+              .eventDate(LocalDateTime.now())
+              .amount(new BigDecimal(123.33))
+              .build();
+        //when
+        TransactionEntity saved = repository.create(toBeSaved);
+        //then
+        assertAll(
+              () -> assertNotNull(saved),
+              () -> assertNotNull(saved.getId()),
+              () -> assertEquals(toBeSaved.getAmount(), saved.getAmount()),
+              () -> assertEquals(toBeSaved.getEventDate(), saved.getEventDate())
+        );
     }
+
+
 }
